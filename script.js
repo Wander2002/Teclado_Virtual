c8980a6dd8af06d5dd16ed940dc0eee09ea8383a
Wander <wanderhanemann@gmail.com>
2024-03-21 20:51:10 -0300

    integração com o banco
    Realizando a integração com o firebase

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAz3bx4u7yQc4enSoYUo7UFHC6etGwr8Yg",
+  authDomain: "teclado-virtual-d446e.firebaseapp.com",
+  databaseURL: "https://teclado-virtual-d446e-default-rtdb.firebaseio.com",
+  projectId: "teclado-virtual-d446e",
+  storageBucket: "teclado-virtual-d446e.appspot.com",
+  messagingSenderId: "925020548911",
+  appId: "1:925020548911:web:5daa46ddfe44f910a1bbda",
+  measurementId: "G-VE3STW0MZY"
+};
+
 function getSenhaFromDatabase() {
     // Simulação de uma consulta ao banco de dados
     return '1234'; // Supondo que a senha seja '1234'
